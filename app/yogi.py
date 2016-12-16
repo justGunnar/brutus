4225500d31af23a6844e880d20e39243cbdad660
Gunnar Bell <gunnar@drizly.com>
2016-12-16 18:22:52 -0500

    adds liquor pipe and makes it work with the POC UI

--- app/yogi.py
+++ app/yogi.py
@@ -15,15 +15,21 @@ from sgd_pipe import SGDPipe
 ###########################
 print 'building brand pipeline'
 brand_pipeline = SGDPipe(
-    '../training_data/items',
-    '../training_data/brands',
+    'training_data/items',
+    'training_data/brands',
     200
 )
 
 print 'building wine pipeline'
 wine_pipeline  = SGDPipe(
-    '../training_data/wines',
-    '../training_data/wine_categories'
+    'training_data/wines',
+    'training_data/wine_categories'
+)
+
+print 'building liquor pipeline'
+liquor_pipeline  = SGDPipe(
+    'training_data/liquors',
+    'training_data/liquor_categories'
 )
 
 print 'ready'
@@ -39,7 +45,6 @@ app = Flask('yogi')
 def home():
     return 'This is the homepage.'
 
-
 @app.route('/brand/<to_predict>', methods=['GET'])
 def brand(to_predict):
     return brand_pipeline.classify([to_predict])[0]
@@ -50,7 +55,10 @@ def brands():
     uniq_brands = set(brands) # uniq!
     return json.dumps(list(uniq_brands))
 
-
 @app.route('/wine_category/<to_predict>', methods=['GET'])
 def wine_category(to_predict):
     return wine_pipeline.classify([to_predict])[0]
+
+@app.route('/liquor_category/<to_predict>', methods=['GET'])
+def liquor_category(to_predict):
+    return liquor_pipeline.classify([to_predict])[0]
