fb2d7b04c65d8bc65d82ad9f372ba8ab9452fd4b
Gunnar Bell <gunnar@drizly.com>
2017-01-12 13:36:40 -0500

    adds a full brand classifier job

---
+++ app/script/full_brand_classifier.py
@@ -0,0 +1,16 @@
+import time
+
+from lib.sgd_pipe      import SGDPipe
+from sklearn.externals import joblib
+
+training_data_path = '../training_data/'
+
+print 'building brand pipeline'
+start = time.time()
+brand_pipeline = SGDPipe(
+    training_data_path + 'items',
+    training_data_path + 'brands'
+)
+end = time.time()
+joblib.dump(brand_pipeline, '../pkl_jar/full_brand_pipeline.pkl')
+print end - start
