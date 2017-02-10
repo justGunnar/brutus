d54dec35e78b2ee0099055d8f16ff634a94a4cc8
Gunnar Bell <gunnar@drizly.com>
2017-02-10 16:12:12 -0500

    new classifiers

--- app/yogi.py
+++ app/yogi.py
@@ -14,9 +14,18 @@ from sklearn.externals   import joblib
 # Startup
 #
 ###########################
-pkl_jar_path = 'pkl-jar/'
-print 'loading brand pipeline'
-brand_pipeline = joblib.load(pkl_jar_path + 'brand_pipeline.pkl')
+pkl_jar_path = 'pkl_jar/'
+print 'loading top level category pipeline'
+top_level_category_pipeline = joblib.load(pkl_jar_path + 'tlc_pipeline.pkl') # TODO: Extra vs Extras??
+
+print 'loading beer brand pipeline'
+beer_brand_pipeline = joblib.load(pkl_jar_path + 'beer_brand_pipeline.pkl')
+
+print 'loading wine brand pipeline'
+wine_brand_pipeline = joblib.load(pkl_jar_path + 'wine_brand_pipeline.pkl')
+
+print 'loading liquor brand pipeline'
+liquor_brand_pipeline = joblib.load(pkl_jar_path + 'liquor_brand_pipeline.pkl')
 
 print 'loading beer pipeline'
 beer_pipeline = joblib.load(pkl_jar_path + 'beer_pipeline.pkl')
@@ -40,15 +49,21 @@ app = Flask('yogi')
 def home():
     return 'This is the homepage.'
 
-@app.route('/brand/<to_predict>', methods=['GET'])
-def brand(to_predict):
-    return brand_pipeline.classify([to_predict])[0]
+@app.route('/top_level_category/<to_predict>', methods=['GET'])
+def top_level_category(to_predict):
+    return top_level_category_pipeline.classify([to_predict])[0]
 
-@app.route('/brands', methods=['GET'])
-def brands():
-    brands      = brand_pipeline._labels
-    uniq_brands = set(brands) # uniq!
-    return json.dumps(list(uniq_brands))
+@app.route('/beer_brand/<to_predict>', methods=['GET'])
+def beer_brand(to_predict):
+    return beer_brand_pipeline.classify([to_predict])[0]
+
+@app.route('/wine_brand/<to_predict>', methods=['GET'])
+def wine_brand(to_predict):
+    return wine_brand_pipeline.classify([to_predict])[0]
+
+@app.route('/liquor_brand/<to_predict>', methods=['GET'])
+def liquor_brand(to_predict):
+    return liquor_brand_pipeline.classify([to_predict])[0]
 
 @app.route('/beer_category/<to_predict>', methods=['GET'])
 def beer_category(to_predict):
@@ -61,3 +76,9 @@ def wine_category(to_predict):
 @app.route('/liquor_category/<to_predict>', methods=['GET'])
 def liquor_category(to_predict):
     return liquor_pipeline.classify([to_predict])[0]
+
+@app.route('/brands', methods=['GET'])
+def brands():
+    brands      = brand_pipeline._labels
+    uniq_brands = set(brands) # uniq!
+    return json.dumps(list(uniq_brands))
